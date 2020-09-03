ac693b7817967ec107ecd074c06520df5c220630
David N <nguyen.david94579@gmail.com>
2020-09-03 00:25:00 -0700

    added login and logout urls path

--- GymBuddyCleaner/GymBuddyCleaner/urls.py
+++ GymBuddyCleaner/GymBuddyCleaner/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register,name ='register' ),
+    path('login/', auth_views.LoginView.as_view(), name ='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name ='logout'),
     path('search/', include('Search.urls')),
 ]
