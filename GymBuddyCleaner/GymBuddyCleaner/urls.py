7e926c4d8affdffb2f9641c3b4116c0e11d731bb
David N <nguyen.david94579@gmail.com>
2020-09-09 16:36:25 -0700

    added general home page for non users and search page for users logged in

--- GymBuddyCleaner/GymBuddyCleaner/urls.py
+++ GymBuddyCleaner/GymBuddyCleaner/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from Users import views as user_views
+from Search import views as search_views
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,6 +25,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', search_views.nonauth_home, name='nonauth-home'),
     path('register/', user_views.register,name ='register' ),
     path('profile/', user_views.profile,name ='profile' ),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name ='login'),
--- GymBuddyCleaner/Search/views.py
+++ GymBuddyCleaner/Search/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def home(request):
-    return render(request, 'Search/search-home.html',{})
+    return render(request, 'Search/search-home.html', {})
+
+def nonauth_home(request):
+    return render(request, 'Search/nonauth-home.html', {})
